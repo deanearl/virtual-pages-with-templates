3b7db1a2cad1db608df534f9aee0f87385589c08
deanearlbartolabac <deanearlbartolabac@gmail.com>
2014-05-12 11:43:40 +0800

    #55 domain mapping issue - fixes

--- virtualpages-with-templates.php
+++ virtualpages-with-templates.php
@@ -807,7 +807,7 @@ if (!class_exists('VirtualPagesTemplates'))
 		{
 			if (function_exists('get_current_blog_id') && function_exists('get_blog_details'))
 			{
-				$this->blog_path = get_blog_details( get_current_blog_id())->path;
+				$this->blog_path = str_replace(get_bloginfo('wpurl'),'',get_bloginfo('url'));
 			}
 			
 			return $this->blog_path;
